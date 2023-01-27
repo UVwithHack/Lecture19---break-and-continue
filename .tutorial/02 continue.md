5e76a891aee1babd2591bf57f5d60c9216085647
UDAY VARA <>
2023-01-27 22:03:25 +0000

    Initial commit

---
+++ .tutorial/02 continue.md
@@ -0,0 +1,18 @@
+## Continue Statement
+The continue statement skips the rest of the loop statements and causes the next iteration to occur.
+## example
+```python
+for i in [2,3,4,6,8,0]:
+    if (i%2!=0):
+        continue
+    print(i)
+  ```
+  ## output
+  ```
+2
+4
+6
+8
+0
+
+```
---
+++ main.py
@@ -0,0 +1,25 @@
+# for i in range(12):
+#     if(i==10):
+#              break
+#     print(5,"*",(i+1),"=",5*(i+1))
+  
+
+# for i in range(12):
+#     if(i==10):
+#       print("Its skip the itreation")
+#       continue                            
+#     print(5,"*",(i),"=",5*(i))
+
+
+# for i in [2,3,4,6,8,0]:
+#     if (i%2!=0):
+#         continue
+#     print(i)
+
+  
+i = 0
+while True:
+  print(i)
+  i = i + 1
+  if(i%10 == 0):
+    break
